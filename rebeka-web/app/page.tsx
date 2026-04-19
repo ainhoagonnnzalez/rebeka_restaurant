11618cd0e53e4cb5377620e7a9f171b8440659ea
ainhoagonnnzalez <ainhoagonnnzalez@gmail.com>
2026-04-19 15:16:25 +0200

    añado proyecto Next rebeka-web

---
+++ rebeka-web/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="bg-black text-white min-h-screen flex items-center justify-center">
+      <h1 className="text-5xl font-bold">
+        Rebeka Restaurant 
+      </h1>
+    </main>
+  );
+}
